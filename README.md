009d46c73059b782cdc7e6a8a2f53114c031039c
Julinium <julinium@gmail.com>
2025-05-27 14:36:48 +0100

    Played with relative paths and .gitignored settings.py

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 This script aims to scrape the PMMP website and store cleaned data in a separate database.
 It's intended for learning pusposes only.
 
-#########
+# Legal ?
 Always remember: scraping may be illegal or cause you to be banned or blacklisted.
 Before scraping a website, please make sure the owner of the website allows it.
+
+# Docker ?
+This application uses Chromium web browser and relies on Cron jobs to update database frequently. It's not pretty practical to run it as a docker container. Instead, it runs well on the server/hypervisor.
--- crony/worker.sh
+++ crony/worker.sh
@@ -3,8 +3,8 @@
 echo "JOB STARTED ||||||||||||||||||||"
 
 _now=$(date +"%Y%m%d-%H%M%S%z")
-_log='/var/opt/seleno/logs/browser.log'
-_lok='/var/opt/seleno/crony/.lock'
+_log='../app/logs/browser.log'
+_lok='./.lock'
 
 if test -e "$_lok"; then
     echo "Execution prevented by a Lock file: $_lok"
@@ -12,7 +12,7 @@ if test -e "$_lok"; then
     echo "The lock file will be removed on next boot. It can also be removed manually."
 else
     touch $_lok
-    /opt/seleno/.venv/bin/python /var/opt/seleno/worker.py "$@" >> "$_log"
+    ../.venv/bin/python ../app/worker.py "$@" >> "$_log"
     echo "Script finished executing. See logs and system journal for details."
     if test -e "$_lok"; then
         echo "Trying to remove Lock file after script finished."
