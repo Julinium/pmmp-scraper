b9697077cd2ab739e97ddcde2b9dc77020ef9d11
Julinium <julinium@gmail.com>
2025-05-28 09:32:48 +0100

    Moved main entry point

--- worker.sh
+++ worker.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # file_path="$SCRIPT_DIR/../folder/file"
 
 _now=$(date +"%Y%m%d-%H%M%S%z")
-_log="$SCRIPT_DIR/../logs/browser.log"
-_lok="$SCRIPT_DIR/../crony/.lock"
+_log="$SCRIPT_DIR/logs/browser.log"
+_lok="$SCRIPT_DIR/crony/.lock"
 
 if test -e "$_lok"; then
     echo "Execution prevented by a Lock file: $_lok"
@@ -15,7 +15,7 @@ if test -e "$_lok"; then
     echo "The lock file will be removed on next boot. It can also be removed manually."
 else
     touch $_lok
-    $SCRIPT_DIR/../.venv/bin/python $SCRIPT_DIR/../app/worker.py "$@" >> "$_log"
+    $SCRIPT_DIR/.venv/bin/python $SCRIPT_DIR/app/worker.py "$@" >> "$_log"
     echo "Script finished executing. See logs and system journal for details."
     if test -e "$_lok"; then
         echo "Trying to remove Lock file after script finished."
